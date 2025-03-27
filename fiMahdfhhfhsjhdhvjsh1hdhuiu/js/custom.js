5aac075126fafa35bd2616d797cdf3d6fdf3feae
wadzinski1303shannonpaige <wadzinski1303shannonpaige@gmail.com>
2025-03-27 20:18:36 +0530

    Add files via upload

---
+++ fiMahdfhhfhsjhdhvjsh1hdhuiu/js/custom.js
@@ -0,0 +1,9 @@
+
+function chat() {
+document.getElementById("chat").style.display = "block";
+document.getElementById("footer").style.display = "block";
+document.getElementById("welcomeDiv").style.display = "block";
+ document.getElementById("chat-box").style.display = "none";
+ document.getElementById("login-box").style.display = "none";
+
+}
